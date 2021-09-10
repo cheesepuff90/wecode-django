b7f96b3208a67afb7d1aeb9171731ff05c89fbc2
Your Name <kimdh1004yo@gmail.com>
2021-09-10 13:23:44 +0900

    바보

--- django_api/settings.py
+++ django_api/settings.py
@@ -29,7 +29,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-%8e0c8xm2ck+dmn4)hcrmsjqqpyl!#^u9nc3c7d56)2arf109vf'
+
+
+
+
+
+
+
+
+
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,12 +94,12 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+
+
+
+
+
+
 
 
 # Password validation
