1defaa45b1f0a394fa9d774056dc0c1bd6bbe6b8
Do Hun Kim <56703088+cheesepuff90@users.noreply.github.com>
2021-09-10 13:26:20 +0900

    바보

--- django_api/settings.py
+++ django_api/settings.py
@@ -28,8 +28,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-%8e0c8xm2ck+dmn4)hcrmsjqqpyl!#^u9nc3c7d56)2arf109vf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,16 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_api.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 
 
 # Password validation
